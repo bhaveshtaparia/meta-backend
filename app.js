ef72e563cd7edeb578f149c402a44b99b544b08b
bhaveshtaparia <bhaveshtaparia7821@gmail.com>
2024-01-31 01:34:40 +0530

    authetication completed

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const dotenv=require('dotenv');
 const cookieParser=require('cookie-parser')
 const bodyParser=require('body-parser');
 const app=express();
+const Signup=require('./router/auth/signupR')
+const Login=require('./router/auth/loginR')
+const Logout=require('./router/auth/logoutR')
 app.use(cookieParser());
 const dbconnection=require('./server');
-
 dotenv.config({path:'./config.env'});
 app.use(express.urlencoded({extended:false}))
 app.use(bodyParser.json());
-dbconnection();
+// dbconnection();
 app.get('/',(req,res)=>{
     res.send("working")
 })
@@ -19,7 +21,9 @@ app.use(cors({
     credentials: true,
     methods:["GET","POST","DELETE","PUT"]
 }));
-
+app.use('/api/v1',Signup);
+app.use('/api/v1',Login);
+app.use('/api/v1',Logout);
 app.listen(process.env.PORT,()=>{
     console.log(`http://localhost:${process.env.PORT}`)
     console.log(`server is working on ${process.env.PORT} `)
